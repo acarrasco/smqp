42187a4a82b4a8b9720ff48d5f03305dfb8f929a
Alejandro Carrasco Yelmo <alejandro.carrasco@aircall.io>
2021-07-05 16:05:34 +0200

    convert Message to class

--- dist/src/Message.js
+++ dist/src/Message.js
@@ -7,79 +7,90 @@ exports.Message = Message;
 
 var _shared = require("./shared");
 
-function Message(fields = {}, content, properties = {}, onConsumed) {
-  let pending = false;
-  let consumedCallback;
-  const messageId = properties.messageId || `smq.mid-${(0, _shared.generateId)()}`;
-  const messageProperties = { ...properties,
-    messageId
-  };
-  const timestamp = messageProperties.timestamp = properties.timestamp || Date.now();
-  let ttl;
+const privateAttributes = {
+  onConsumed: Symbol('onConsumed'),
+  pending: Symbol('pending'),
+  messageId: Symbol('messageId'),
+  ttl: Symbol('ttl'),
+  consumedCallback: Symbol('consumedCallback')
+};
+const publicMethods = ['consume', 'ack', 'nack', 'reject'];
 
-  if (properties.expiration) {
-    ttl = messageProperties.ttl = timestamp + parseInt(properties.expiration);
-  }
+class _Message {
+  constructor(fields = {}, content, properties = {}, onConsumed) {
+    this[privateAttributes.onConsumed] = onConsumed;
+    this[privateAttributes.pending] = false;
+    this[privateAttributes.messageId] = properties.messageId || `smq.mid-${(0, _shared.generateId)()}`;
+    const messageProperties = { ...properties,
+      messageId: this[privateAttributes.messageId]
+    };
+    const timestamp = messageProperties.timestamp = properties.timestamp || Date.now();
 
-  const message = {
-    fields: { ...fields,
+    if (properties.expiration) {
+      this[privateAttributes.ttl] = messageProperties.ttl = timestamp + parseInt(properties.expiration);
+    }
+
+    this.fields = { ...fields,
       consumerTag: undefined
-    },
-    content,
-    properties: messageProperties,
-
-    get messageId() {
-      return messageId;
-    },
-
-    get ttl() {
-      return ttl;
-    },
-
-    get consumerTag() {
-      return message.fields.consumerTag;
-    },
-
-    get pending() {
-      return pending;
-    },
-
-    consume,
-    ack,
-    nack,
-    reject
-  };
-  return message;
-
-  function consume({
-    consumerTag
-  } = {}, consumedCb) {
-    pending = true;
-    message.fields.consumerTag = consumerTag;
-    consumedCallback = consumedCb;
-  }
-
-  function reset() {
-    pending = false;
-  }
-
-  function ack(allUpTo) {
-    if (!pending) return;
-    consumed('ack', allUpTo);
-  }
-
-  function nack(allUpTo, requeue = true) {
-    if (!pending) return;
-    consumed('nack', allUpTo, requeue);
-  }
-
-  function reject(requeue = true) {
-    nack(false, requeue);
-  }
-
-  function consumed(operation, allUpTo, requeue) {
-    [consumedCallback, onConsumed, reset].forEach(fn => {
-      if (fn) fn(message, operation, allUpTo, requeue);
+    };
+    this.content = content;
+    this.properties = messageProperties;
+    publicMethods.forEach(fn => {
+      this[fn] = _Message.prototype[fn].bind(this);
     });
   }
+
+  get messageId() {
+    return this[privateAttributes.messageId];
+  }
+
+  get ttl() {
+    return this[privateAttributes.ttl];
+  }
+
+  get consumerTag() {
+    return this.fields.consumerTag;
+  }
+
+  get pending() {
+    return this[privateAttributes.pending];
+  }
+
+  consume({
+    consumerTag
+  } = {}, consumedCb) {
+    this[privateAttributes.pending] = true;
+    this.fields.consumerTag = consumerTag;
+    this[privateAttributes.consumedCallback] = consumedCb;
+  }
+
+  reset() {
+    this[privateAttributes.pending] = false;
+  }
+
+  ack(allUpTo) {
+    if (this[privateAttributes.pending]) {
+      this.consumed('ack', allUpTo);
+    }
+  }
+
+  nack(allUpTo, requeue = true) {
+    if (!this[privateAttributes.pending]) return;
+    this.consumed('nack', allUpTo, requeue);
+  }
+
+  reject(requeue = true) {
+    this.nack(false, requeue);
+  }
+
+  consumed(operation, allUpTo, requeue) {
+    [this[privateAttributes.consumedCallback], this[privateAttributes.onConsumed], this.reset.bind(this)].forEach(fn => {
+      if (fn) fn(this, operation, allUpTo, requeue);
+    });
+  }
+
+}
+
+function Message(fields = {}, content, properties = {}, onConsumed) {
+  return new _Message(fields, content, properties, onConsumed);
 }
--- src/Message.js
+++ src/Message.js
@@ -1,70 +1,91 @@
-import {generateId} from './shared';
+import { generateId } from './shared';
 
-export {Message};
+export { Message };
 
-function Message(fields = {}, content, properties = {}, onConsumed) {
-  let pending = false;
-  let consumedCallback;
+const privateAttributes = {
+  onConsumed: Symbol('onConsumed'),
+  pending: Symbol('pending'),
+  messageId: Symbol('messageId'),
+  ttl: Symbol('ttl'),
+  consumedCallback: Symbol('consumedCallback'),
+};
+const publicMethods = ['consume', 'ack', 'nack', 'reject'];
 
-  const messageId = properties.messageId || `smq.mid-${generateId()}`;
-  const messageProperties = {...properties, messageId};
-  const timestamp = messageProperties.timestamp = properties.timestamp || Date.now();
-  let ttl;
-  if (properties.expiration) {
-    ttl = messageProperties.ttl = timestamp + parseInt(properties.expiration);
-  }
+class _Message {
+  constructor(fields = {}, content, properties = {}, onConsumed) {
 
-  const message = {
-    fields: {...fields, consumerTag: undefined},
-    content,
-    properties: messageProperties,
-    get messageId() {
-      return messageId;
-    },
-    get ttl() {
-      return ttl;
-    },
-    get consumerTag() {
-      return message.fields.consumerTag;
-    },
-    get pending() {
-      return pending;
-    },
-    consume,
-    ack,
-    nack,
-    reject,
-  };
+    this[privateAttributes.onConsumed] = onConsumed;
+    this[privateAttributes.pending] = false;
+    this[privateAttributes.messageId] = properties.messageId || `smq.mid-${generateId()}`;
 
-  return message;
+    const messageProperties = { ...properties, messageId: this[privateAttributes.messageId] };
+    const timestamp = (messageProperties.timestamp =
+      properties.timestamp || Date.now());
+    if (properties.expiration) {
+      this[privateAttributes.ttl] = messageProperties.ttl =
+        timestamp + parseInt(properties.expiration);
+    }
 
-  function consume({consumerTag} = {}, consumedCb) {
-    pending = true;
-    message.fields.consumerTag = consumerTag;
-    consumedCallback = consumedCb;
-  }
-
-  function reset() {
-    pending = false;
-  }
-
-  function ack(allUpTo) {
-    if (!pending) return;
-    consumed('ack', allUpTo);
-  }
-
-  function nack(allUpTo, requeue = true) {
-    if (!pending) return;
-    consumed('nack', allUpTo, requeue);
-  }
-
-  function reject(requeue = true) {
-    nack(false, requeue);
-  }
-
-  function consumed(operation, allUpTo, requeue) {
-    [consumedCallback, onConsumed, reset].forEach((fn) => {
-      if (fn) fn(message, operation, allUpTo, requeue);
+    this.fields = { ...fields, consumerTag: undefined };
+    this.content = content;
+    this.properties = messageProperties;
+    publicMethods.forEach((fn) => {
+      this[fn] = _Message.prototype[fn].bind(this);
     });
   }
+
+  get messageId() {
+    return this[privateAttributes.messageId];
+  }
+
+  get ttl() {
+    return this[privateAttributes.ttl];
+  }
+
+  get consumerTag() {
+    return this.fields.consumerTag;
+  }
+
+  get pending() {
+    return this[privateAttributes.pending];
+  }
+
+  consume({ consumerTag } = {}, consumedCb) {
+    this[privateAttributes.pending] = true;
+    this.fields.consumerTag = consumerTag;
+    this[privateAttributes.consumedCallback] = consumedCb;
+  }
+
+  reset() {
+    this[privateAttributes.pending] = false;
+  }
+
+  ack(allUpTo) {
+    if (this[privateAttributes.pending]) {
+      this.consumed('ack', allUpTo);
+    }
+  }
+
+  nack(allUpTo, requeue = true) {
+    if (!this[privateAttributes.pending]) return;
+    this.consumed('nack', allUpTo, requeue);
+  }
+
+  reject(requeue = true) {
+    this.nack(false, requeue);
+  }
+
+  consumed(operation, allUpTo, requeue) {
+    [
+      this[privateAttributes.consumedCallback],
+      this[privateAttributes.onConsumed],
+      this.reset.bind(this),
+    ].forEach((fn) => {
+      if (fn) fn(this, operation, allUpTo, requeue);
+    });
+  }
+}
+
+function Message(fields = {}, content, properties = {}, onConsumed) {
+  return new _Message(fields, content, properties, onConsumed);
 }
